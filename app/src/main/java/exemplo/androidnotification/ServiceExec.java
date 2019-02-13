10543654ed09efc6f5fbbe74ca4de738eb12654a
carlosbezerravilela@gmail.com <7#@mick@#7>
2019-02-13 20:17:37 -0200

    criação do Activity Notification - criação e chamada da notificação

--- app/src/main/java/exemplo/androidnotification/ServiceExec.java
+++ app/src/main/java/exemplo/androidnotification/ServiceExec.java
@@ -1,7 +1,11 @@
 package exemplo.androidnotification;
 
 import android.app.IntentService;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.content.Intent;
+import android.widget.RemoteViews;
 import android.widget.Toast;
 
 public class ServiceExec extends IntentService {
@@ -19,6 +23,36 @@ public class ServiceExec extends IntentService {
 
 
 
+        Notification foregroundNote;
+
+        RemoteViews bigView = new RemoteViews(getApplicationContext().getPackageName(),
+                R.layout.activity_notification);
+
+// bigView.setOnClickPendingIntent() etc..
+
+        Notification.Builder mNotifyBuilder = new Notification.Builder(this);
+        foregroundNote = mNotifyBuilder.setContentTitle("some string")
+
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setWhen(System.currentTimeMillis())
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setTicker("Hearty365")
+                //     .setPriority(Notification.PRIORITY_MAX)
+                .setContentTitle("Default notification")
+                .setContentText("Lorem ipsum dolor sit amet, consectetur adipiscing elit.")
+                .setContentInfo("Info")
+                .setOngoing(true)
+                .build();
+
+        foregroundNote.bigContentView = bigView;
+
+// now show notification..
+        NotificationManager mNotifyManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotifyManager.notify(1, foregroundNote);
+
 
     }
+
+
+
 }
